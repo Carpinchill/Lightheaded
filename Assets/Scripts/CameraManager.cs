2c0c4a3aef36a60f72bd017a6bfa91c4e9bacf96
beluelu <beludurannn@gmail.com>
2024-09-26 21:08:47 -0300

    Menu

--- Assets/Scripts/CameraManager.cs
+++ Assets/Scripts/CameraManager.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class PlayerController : MonoBehaviour
+public class CameraManager : MonoBehaviour
 {
     public Transform target;               // Referencia al jugador
     public Vector3 normalOffset;           // Offset normal de la cámara
